cf21e0e11a7802097b29cc22ba5c852500560594
kennethgoodman <kennethsgoodman@yahoo.com>
2015-11-12 20:48:25 -0500

    took out stormpath

--- index.js
+++ index.js
@@ -37,11 +37,12 @@ app.get('/search', function(request,response){
     response.render('pages/search', {classes: x})
   })
 })
+/*
 app.use(stormpath.init(app, {
   apiKeyId:     process.env.STORMPATH_API_KEY_ID,
   apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
   secretKey:    process.env.STORMPATH_SECRET_KEY,
   application:  process.env.STORMPATH_URL,
-}));
+}));*/
 
 //app.listen(process.env.PORT || 5000);
