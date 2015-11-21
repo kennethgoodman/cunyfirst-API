a0bbcf33e091cc9ea40992634ac45bb334c5792f
kennethgoodman <kennethsgoodman@yahoo.com>
2015-11-20 23:16:08 -0500

    TESting

--- bin/test.js
+++ bin/test.js
@@ -1,5 +1,9 @@
-function sayHello() {
-  console.log('Hello');
-}
-sayHello();
-exit();
+var Heroku = require('heroku-client'),
+    heroku = new Heroku({ token: "a6e0511d-0109-4209-b8c3-31f3f9e02273" });
+
+var app = heroku.apps('noclosedclass');
+
+app.info(function (err, app) {
+	console.log(app)
+  // Details about the `app`
+});
