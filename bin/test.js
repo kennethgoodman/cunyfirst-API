36f1f3064bf71299c71f61f7c936d24824eaec2c
kennethgoodman <kennethsgoodman@yahoo.com>
2015-11-20 23:50:21 -0500

    Testing

--- bin/test.js
+++ bin/test.js
@@ -1,9 +1,9 @@
-var Heroku = require('heroku-client'),
-    heroku = new Heroku({ token: "a6e0511d-0109-4209-b8c3-31f3f9e02273" });
+var Heroku = require('heroku.node');
 
-var app = heroku.apps('noclosedclass');
+var client = new Heroku({email: 'kennethsgoodman@yahoo.com', api_key: 'a6e0511d-0109-4209-b8c3-31f3f9e02273'});
+// Do something with client
 
-app.info(function (err, app) {
-	console.log(app)
-  // Details about the `app`
-});
+client.app('noclosedclass').dynos.list(function(err,dyno){
+	client.app('noclosedclass').dyno(dyno[0].id).restart(function(e,f,g){
+	})
+})
