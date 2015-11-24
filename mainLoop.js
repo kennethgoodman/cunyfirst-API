81e878c5fb5b164d5278ff567449ac0d0df43f8c
kennethgoodman <kennethsgoodman@yahoo.com>
2015-11-24 13:22:25 -0500

    took out log for classes status if closed

--- mainLoop.js
+++ mainLoop.js
@@ -89,7 +89,7 @@ queueRead2 = function lambda(){
 				sendQuery2(q, params, function(row){
 					try{
 						var text = item.dept + ": "+ row["class"] +', ' + row["section"] + ' is ' + struct[row["class"]][row["section"]]["Status"] + ". Teacher: " + struct[row["class"]][row["section"]]['Instructor'];
-						console.log(new Date() + ": " + text)
+						//console.log(new Date() + ": " + text)
 					} catch(err){
 						console.log(err)
 					}
