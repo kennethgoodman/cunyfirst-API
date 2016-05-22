381ac5407f32fa6dcfcc6c958e763e68f01d0b03
kennethgoodman <kennethsgoodman@yahoo.com>
2016-05-22 13:27:41 -0400

    parsers redone

--- worker 2.js
+++ worker 2.js
@@ -23,6 +23,28 @@ Array.prototype.remove = function(from, to) {
   return this.push.apply(this, rest);
 };
 
+var parseDropdownOptions = function(body, selectIndexString, callback){
+    var selectIndex = body.indexOf(selectIndexString)
+    var selectHtml = body.substring(selectIndex)
+    var selectHtml = selectHtml.substring(selectHtml.indexOf("<option"), selectHtml.indexOf("</select>"))
+    
+    optionsString = selectHtml.split('<option value=').join("")
+    optionsString = optionsString.split('></option>').join("")
+    optionsString = optionsString.split('</option>').join("")
+    optionsString = optionsString.split("selected").join("")
+    optionsString = optionsString.split('\"').join("")
+    optionsString = optionsString.split("=").join("")
+    optionsString = optionsString.split("\'").join("")
+    
+    var options = optionsString.split("\n")
+    valueTextStruct = {}
+    for(var i = 1; i < options.length - 1 ; i++){
+        temp = options[i].split(">")
+        valueTextStruct[temp[1]] = temp[0]
+    }
+    callback(valueTextStruct)
+}
+
 var classesArray = [];
 var urlProducer = function (icsid, icstate, inst, session){
     return 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/GUEST/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL?ICAJAX=1&ICNAVTYPEDROPDOWN=0&ICType=Panel&ICElementNum=0&ICStateNum='
@@ -63,8 +85,78 @@ getSections = function (inst, session, dept, callback){
         request.get(submit_options, function (err, res, body){
             request.get(submit_options, function(err, res, body){
                 submit_options['url']= urlProducerClasses(key, '2', session, dept)
-                request.get(submit_options, function (err, res, body){
-                    console.log(body)
+                request.get(submit_options, function (err, res, body){ 
+                    body = body.substring(body.indexOf("win0divSSR_CLSRSLT_WRK_GROUPBOX2$"))
+                    bodySplit = body.split("win0divSSR_CLSRSLT_WRK_GROUPBOX2$") //split by section
+                    for(var i = 1; i < bodySplit.length - 1; i++){
+                        var temp = bodySplit[i]
+                        //get class:
+                        var classNumber = temp.substring(temp.indexOf("/a>")+3, temp.indexOf("</DIV>"))
+                        console.log(classNumber.split("&nbsp;").join(""))
+                        //get class
+                        var tableWithAllClasses = temp.substring(temp.indexOf("ACE_$ICField48"))
+                        console.log("--------")
+                        rowsSplit = tableWithAllClasses.split("ACE_SSR_CLSRSLT_WRK_GROUPBOX3")
+                        for(var j = 1; j < rowsSplit.length - 1; j++){
+                            var temp2 = rowsSplit[j]
+
+                            var section = temp2.substr(temp2.indexOf("win0divMTG_CLASS_NBR"))
+                            var section = section.substr(section.indexOf("</a>") - 5, 5)
+                            console.log(section)
+
+                            var className = temp2.substr(temp2.indexOf("win0divMTG_CLASSNAME"))
+                            var className = className.substr(className.indexOf("</a>")-25,25)
+                            var className = className.substring(className.indexOf(">")+1, className.indexOf("<"))
+                            console.log("--------")
+                            console.log(className)
+
+                            var dayTime = temp2.substr(temp2.indexOf("win0divMTG_DAYTIME"))
+                            var dayTime = dayTime.substr(dayTime.indexOf("</span>")-25, 50)
+                            var dayTime = dayTime.substring(dayTime.indexOf(">")+1, dayTime.indexOf("<")) 
+                            console.log("--------")
+                            console.log(dayTime)
+
+
+                            var room = temp2.substr(temp2.indexOf("win0divMTG_ROOM"))
+                            var room = room.substr(room.indexOf("</span>")-25, 50)
+                            var room = room.substring(room.indexOf(">")+1, room.indexOf("<")) 
+                            console.log("--------")
+                            console.log(room)
+
+                            var teacher = temp2.substr(temp2.indexOf("win0divMTG_INSTR"))
+                            var teacher = teacher.substr(teacher.indexOf("</span>")-25, 50)
+                            var teacher = teacher.substring(teacher.indexOf(">")+1, teacher.indexOf("<")) 
+                            console.log("--------")
+                            console.log(teacher)
+
+                            var meetingTimes = temp2.substr(temp2.indexOf("win0divMTG_TOPIC"))
+                            var meetingTimes = meetingTimes.substr(meetingTimes.indexOf("</span>")-25, 50)
+                            var meetingTimes = meetingTimes.substring(meetingTimes.indexOf(">")+1, meetingTimes.indexOf("<")) 
+                            console.log("--------")
+                            console.log(meetingTimes)
+
+
+                            var status = temp2.substr(temp2.indexOf("win0divDERIVED_CLSRCH_SSR_STATUS_LONG"))
+                            var status = status.substr(status.indexOf("alt=") + 5, 12)
+                            var status = status.substring(0, status.indexOf("\"")) 
+                            console.log("--------")
+                            console.log(status)
+
+                            var core = temp2.substr(temp2.indexOf("win0divDERIVED_CLSRCH_DESCRLONG"))
+                            var core = core.substring(core.indexOf("<span")+1,core.indexOf("</span>"))
+                            var core = core.substring(core.indexOf(">")+1)
+                            var core = core.replace("amp;","") 
+                            console.log("--------")
+                            console.log(core)
+                            break
+                        }
+                        break
+                        // //bodySplit[i] = bodySplit.substring(bodySplit.indexOf("ACE_$ICField48$"))
+                        // //
+                        // bodySplit[i] = bodySplit.substring(bodySplit.indexOf("<tbody>"),bodySplit.indexOf("</tbody>"))
+                        // rowSplit = bodySplit[i].split("<tr>")
+                        // console.log(bodySplit[i])
+                    }
                     //parse this for subjects
                 })
             })
@@ -72,9 +164,9 @@ getSections = function (inst, session, dept, callback){
         })
     })
 }
-/*getSections ('QNS01', '1169', 'CSCI', function (){
-    console.log();
-})*/
+getSections ('QNS01', '1169', 'MATH', function (){
+    //console.log();
+})
 
 getDept = function(inst, session, callback){
 	request.post(options, function(err, res, body) {
@@ -91,16 +183,12 @@ getDept = function(inst, session, callback){
         }
         request.get(submit_options, function (err, res, body){
             request.get(submit_options, function (err, res, body){
-                console.log(body)
-                //parse this for subjects
+                var selectIndexString = "select name='SSR_CLSRCH_WRK_SUBJECT_SRCH$"
+                parseDropdownOptions(body, selectIndexString, callback)
             })
         })
     })
 }
-/*getDept('QNS01', '1162', function(result){
-    console.log('hii')
-})*/
-
 getSession = function (inst, callback){
     request.post(options, function(err, res, body) {
         if(err) {
@@ -116,14 +204,11 @@ getSession = function (inst, callback){
             jar: options.jar
         };
         request.get(submit_options, function(err,res,body){
-            //parse this for session
+            var selectIndexString = "id='CLASS_SRCH_WRK2_STRM$35$"
+            parseDropdownOptions(body, selectIndexString, callback)
         })
     })
 }
-
-/*getSession('QNS01', function(result){
-    console.log(result)
-})*/
 
 getInst = function(callback){
     request.post(options, function(err, res, body) {
@@ -143,38 +228,8 @@ getInst = function(callback){
                 logger.error(err)
                 return
             }
-            data = data.split('<option value=').join("")
-            data = data.split('></option>').join("")
-            data = data.split('</option>').join("")
-            data = data.split('\"').join("")
-            data = data.match(/[^\s]+/g);
-            var schools = []
-            var schoolNames = []
-            var schoolName = ""
-            for(var e = 1; e < data.length -1; e++){
-                if(data[e].indexOf(">") == 5){
-                    schools.push(data[e].substring(0,5))
-                    schoolNames.push(schoolName)
-                    schoolName = ""
-                    data[e] = data[e].substring(6);
-                }
-                schoolName += " " + data[e]
-            }
-            schoolNames.push(schoolName)
-            schoolNames.remove(0)
-            var schoolStruct = {}
-            for(var e in schools){
-                if(e == schools.length-1) break;
-                schoolStruct[schools[e]] = schoolNames[e].substring(1)
-            }
-            schoolStruct[schools[schools.length-1]] = schoolNames[schoolNames.length-1].substring(1)
-            callback(schoolStruct);
+            var selectIndexString = "select name='CLASS_SRCH_WRK2_INSTITUTION$31$"
+            parseDropdownOptions(body, selectIndexString, callback)
         })
     })
 }
-
-getInst(function(r){
-    console.log(r)
-})
-
-
